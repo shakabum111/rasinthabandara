b6d164ba99a74df500513a35ca15f9b138bb9aab
shakabum111 <137705725+shakabum111@users.noreply.github.com>
2024-05-14 08:42:37 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,46 +1,57 @@
 <?php
-
+// Include the database configuration file
 include 'config.php';
 
+// Initialize an empty array to store error/success messages
+$message = array();
+
+// Check if the form is submitted
 if(isset($_POST['submit'])){
-
-   $name = $_POST['name'];
-   $name = filter_var($name, FILTER_SANITIZE_STRING);
-   $email = $_POST['email'];
-   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   
+   // Sanitize and retrieve form inputs
+   $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+   $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
    $pass = md5($_POST['pass']);
-   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
    $cpass = md5($_POST['cpass']);
-   $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
-
    $image = $_FILES['image']['name'];
    $image_tmp_name = $_FILES['image']['tmp_name'];
    $image_size = $_FILES['image']['size'];
    $image_folder = 'uploaded_img/'.$image;
 
+   // Check if the email already exists in the database
    $select = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
    $select->execute([$email]);
 
+   // If email already exists, add a message to the array
    if($select->rowCount() > 0){
-      $message[] = 'user already exist!';
-   }else{
+      $message[] = 'User already exists!';
+   } else {
+      // If email doesn't exist, proceed with registration
+
+      // Check if passwords match
       if($pass != $cpass){
-         $message[] = 'confirm password not matched!';
-      }elseif($image_size > 2000000){
-         $message[] = 'image size is too large!';
-      }else{
+         $message[] = 'Confirm password not matched!';
+      } elseif($image_size > 2000000){
+         // Check if image size is too large
+         $message[] = 'Image size is too large!';
+      } else {
+         // Insert user data into the database
          $insert = $conn->prepare("INSERT INTO `users`(name, email, password, image) VALUES(?,?,?,?)");
-         $insert->execute([$name, $email, $cpass, $image]);
-         if($insert){
+         if($insert->execute([$name, $email, $cpass, $image])){
+            // If data insertion is successful, move uploaded image to the specified folder
             move_uploaded_file($image_tmp_name, $image_folder);
-            $message[] = 'registered succesfully!';
+            // Add success message to the array
+            $message[] = 'Registered successfully!';
+            // Redirect to login page
             header('location:login.php');
+            exit(); // Ensure script stops executing after redirection
+         } else {
+            // If data insertion fails, add an error message to the array
+            $message[] = 'Registration failed. Please try again!';
          }
       }
    }
-
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -49,7 +60,7 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>register</title>
+   <title>Register</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -60,34 +71,31 @@ if(isset($_POST['submit'])){
 </head>
 <body>
 
-
-
 <?php
-   if(isset($message)){
-      foreach($message as $message){
-         echo '
-         <div class="message">
-            <span>'.$message.'</span>
-            <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
-         </div>
-         ';
-      }
+// Display error/success messages
+if(!empty($message)){
+   foreach($message as $msg){
+      echo '
+      <div class="message">
+         <span>'.$msg.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
    }
+}
 ?>
    
 <section class="form-container">
-
-   <form action="" method="post" enctype="multipart/form-data">
-      <h3>register now</h3>
-      <input type="text" required placeholder="enter your username" class="box" name="name">
-      <input type="email" required placeholder="enter your email" class="box" name="email">
-      <input type="password" required placeholder="enter your password" class="box" name="pass">
-      <input type="password" required placeholder="confirm your password" class="box" name="cpass">
+   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+      <h3>Register Now</h3>
+      <input type="text" required placeholder="Enter your username" class="box" name="name">
+      <input type="email" required placeholder="Enter your email" class="box" name="email">
+      <input type="password" required placeholder="Enter your password" class="box" name="pass">
+      <input type="password" required placeholder="Confirm your password" class="box" name="cpass">
       <input type="file" name="image" required class="box" accept="image/jpg, image/png, image/jpeg">
-      <p>already have an account? <a href="login.php">login now</a></p>
-      <input type="submit" value="register now" class="btn" name="submit">
+      <p>Already have an account? <a href="login.php">Login now</a></p>
+      <input type="submit" value="Register Now" class="btn" name="submit">
    </form>
-
 </section>
 
 </body>
