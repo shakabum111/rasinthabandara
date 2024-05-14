0fdb15c61035f5ca452e28e7efee3cd95c96b444
shakabum111 <137705725+shakabum111@users.noreply.github.com>
2024-05-14 08:33:54 +0530

    Update register.php

--- register.php
+++ register.php
@@ -55,7 +55,7 @@ if(isset($_POST['submit'])){
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="style22.css">
 
 </head>
 <body>
